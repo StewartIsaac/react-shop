7b25463d10c6f4877c3590d7baa0d9a045160ab0
Gabriel Isaac <stewartisaac90@gmail.com>
2024-09-07 09:47:19 +0100

    setup project

---
+++ react-shop-app/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: "media", // or false or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
