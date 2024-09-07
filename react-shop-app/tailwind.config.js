7726eb9f61b43a094ff8593df9f038ddc78aeec6
Gabriel Isaac <stewartisaac90@gmail.com>
2024-09-07 13:19:45 +0100

    customising theme colors

--- react-shop-app/tailwind.config.js
+++ react-shop-app/tailwind.config.js
@@ -1,8 +1,14 @@
+const colors = require("tailwindcss/colors");
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: colors.slate,
+        red: colors.rose,
+      },
+    },
   },
   darkMode: "media", // or false or 'class',
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
