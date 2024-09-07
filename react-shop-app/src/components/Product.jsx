478b10663d6d38855032381e316ce6a8145179e2
Gabriel Isaac <stewartisaac90@gmail.com>
2024-09-07 14:58:20 +0100

    adapt product image to dark theme

--- react-shop-app/src/components/Product.jsx
+++ react-shop-app/src/components/Product.jsx
@@ -4,13 +4,13 @@ const Product = ({
   product: { title, price, description, category, image },
 }) => {
   return (
-    <div className="bg-white w-full mb-5 flex shadow-sm rounded-lg">
-      <div className="w-5/12 p-2">
+    <div className="bg-white w-full mb-5 flex shadow-sm rounded-lg dark:bg-gray-300">
+      <div className="w-5/12 p-2 dark:bg-white rounded-tl-lg rounded-bl-lg">
         <div style={{ backgroundImage: `url(${image})` }} className="bg-contain bg-no-repeat bg-center w-full h-full"></div>
       </div>
       <div className="w-7/12 p-5">
         <h1 className="md:text-2xl">{title.substr(0, 50)}</h1>
-        <h3 className="text-gray-400">{category}</h3>
+        <h3 className="text-gray-400 dark:text-gray-700">{category}</h3>
         <p className="md:text-4xl sm:text-xl text-red-500 mt-4">${price}</p>
         <div className="mt-4">{description.substr(0, 100)}</div>
       </div>
